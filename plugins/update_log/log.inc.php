841bef32c5185a84c0fc99c9ffdc959b52dccb46
kookxiang <i@ikk.me>
2004-01-15 03:44:35 +0800

    remove update_log plugin

--- plugins/update_log/log.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('IN_KKFRAME')) exit('Access Denied!');
-?>
-<h2>新功能推荐</h2>
-<script type="text/javascript">
-if (typeof defered_js == 'undefined') var defered_js = new Array;
-defered_js.push('//api.ikk.me/tieba_sign_update_log.js');
-</script>
--- plugins/update_log/plugin.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('IN_KKFRAME')) exit('Access Denied!');
-class plugin_update_log{
-	var $description = '前台“新功能推荐”菜单，告诉用户有什么新鲜的功能';
-	var $modules = array(
-		array('id' => 'log', 'type' => 'page', 'title' => '新功能推荐', 'file' => 'log.inc.php'),
-	);
-}
