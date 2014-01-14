aef4ef94793c4dd6804ee8784cad6e42cdb379a4
kookxiang <i@ikk.me>
2014-01-15 02:09:16 +0800

    save adminid into database during updating

--- system/function/updater/1.13.12.25.php
+++ system/function/updater/1.13.12.25.php
@@ -1,5 +1,6 @@
 <?php
 if(!defined('IN_KKFRAME')) exit('Access Denied');
+if($_config['adminid']) saveSetting('admin_uid', $_config['adminid']);
 saveSetting('version', '1.14.1.15');
 showmessage('成功更新到 1.14.1.15！', './');
 ?>
