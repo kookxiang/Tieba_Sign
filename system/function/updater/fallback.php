928026c290aec8ead5798facb03456bc0b5fb73c
kookxiang <i@ikk.me>
2014-01-15 04:31:57 +0800

    fix bug while update from old version

--- system/function/updater/fallback.php
+++ system/function/updater/fallback.php
@@ -1,13 +1,11 @@
 <?php
 if(!defined('IN_KKFRAME')) exit('Access Denied');
-$query = DB::query('SHOW TABLES');
-$tables = array();
-while($table = DB::fetch($query)){
-	$tables[] = implode('', $table);
-}
-
-if(!in_array('member', $tables)){
-	include SYSTEM_ROOT.'./function/updater/install.php';
+if(!$current_version){
+	if(getSetting('version')){
+		saveVersion(getSetting('version'));
+		header('Location: ./');
+		exit();
+	}
 }
 throw new Exception("找不到更新程序，无法进行更新！<br>Error while upgrade from version {$current_version} to version ".VERSION.'.');
 ?>
