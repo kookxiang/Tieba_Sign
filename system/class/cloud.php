2576acc40753a5943fd97025e26f8d359fea3f83
kookxiang <i@ikk.me>
2014-01-17 23:50:06 +0800

    fix bugs

--- system/class/cloud.php
+++ system/class/cloud.php
@@ -13,12 +13,12 @@ class cloud {
 		if (!$api_name) throw new Exception('Request remote api failed: empty request!');
 		$parms = func_get_args();
 		unset($parms[0]);
-		$parm_string = json_encode($parms);
+		$parm_string = serialize($parms);
 		$parm_string = authcode($parm_string, self::key());
 		$parm_string = bin2hex($parm_string);
 		$res = fetch_url(self::API_ROOT."{$api_name}.php?sid=".self::id(), 0, 'parm='.$parm_string);
 		if (!$res) throw new Exception('Request remote api failed: empty response!');
-		$ret = json_decode($res);
+		$ret = unserialize($res);
 		if (!$ret) throw new Exception('Request remote api failed: decode fail');
 		return $ret;
 	}
