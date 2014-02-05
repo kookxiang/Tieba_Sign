997b45c9972010464eac89a055ece238e8c53022
kookxiang <i@ikk.me>
2014-02-06 01:50:04 +0800

    upgrade register limit plugin

--- plugins/register_limit/plugin.class.php
+++ plugins/register_limit/plugin.class.php
@@ -1,11 +1,10 @@
 <?php
 if(!defined('IN_KKFRAME')) exit('Access Denied!');
-class plugin_register_limit{
+class plugin_register_limit extends Plugin {
 	var $description = '限制单个 IP 的注册上限';
 	var $modules = array();
 	function on_install(){
 		DB::query('CREATE TABLE IF NOT EXISTS `kk_ip_limit` ( `a` tinyint(3) unsigned NOT NULL, `b` tinyint(3) unsigned NOT NULL, `c` tinyint(3) unsigned NOT NULL, `d` tinyint(3) unsigned NOT NULL, `count` tinyint(3) unsigned NOT NULL, `lastact` int(10) unsigned NOT NULL, UNIQUE KEY `ip` (`a`,`b`,`c`,`d`)) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_bin;');
-		saveSetting('ip_reglimit', 5);
 	}
 	function on_uninstall(){
 		DB::query('DROP TABLE kk_ip_limit');
@@ -22,7 +21,7 @@ class plugin_register_limit{
 		$time = TIMESTAMP;
 		DB::query("DELETE FROM kk_ip_limit WHERE lastact<{$time}-86400");
 		if($count > 0) DB::query("UPDATE kk_ip_limit SET lastact='{$time}' WHERE a='{$a}' AND b='{$b}' AND c='{$c}' AND d='{$d}'");
-		if($count >= getSetting('ip_reglimit')) showmessage('达到单 IP 注册上限，禁止注册。', dreferer());
+		if($count >= $this->getSetting('ip_reglimit', 5)) showmessage('达到单 IP 注册上限，禁止注册。', dreferer());
 		if($count > 0){
 			DB::query("UPDATE kk_ip_limit SET count=count+1 WHERE a='{$a}' AND b='{$b}' AND c='{$c}' AND d='{$d}'");
 		}else{
@@ -31,10 +30,10 @@ class plugin_register_limit{
 	}
 	function on_config(){
 		if($_POST['limit']){
-			saveSetting('ip_reglimit', $_POST['limit']);
+			$this->saveSetting('ip_reglimit', $_POST['limit']);
 			showmessage('设置已经保存！');
 		}else{
-			return '<p>单个 IP 注册上限：<input type="text" name="limit" value="'.getSetting('ip_reglimit').'" /></p>';
+			return '<p>单个 IP 注册上限：<input type="text" name="limit" value="'.$this->getSetting('ip_reglimit', 5).'" /></p>';
 		}
 	}
 }
