7ae262dac743edbfe24464a53ab99985c58cdbe0
kookxiang <kookxiang@qq.com>
2014-04-14 19:43:15 +0800

    add xml parse function

--- system/function/core.php
+++ system/function/core.php
@@ -351,6 +351,25 @@ function fsocketopen($hostname, $port = 80, &$errno, &$errstr, $timeout = 15) {
 	}
 	return $fp;
 }
+function xml2array(&$xml, $isnormal = FALSE) {
+	$xml_parser = new XMLparse($isnormal);
+	$data = $xml_parser->parse($xml);
+	$xml_parser->destruct();
+	return $data;
+}
+function array2xml($arr, $htmlon = TRUE, $isnormal = FALSE, $level = 1) {
+	$s = $level == 1 ? "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<root>\r\n" : '';
+	$space = str_repeat("\t", $level);
+	foreach($arr as $k => $v) {
+		if(!is_array($v)) {
+			$s .= $space."<item id=\"$k\">".($htmlon ? '<![CDATA[' : '').$v.($htmlon ? ']]>' : '')."</item>\r\n";
+		} else {
+			$s .= $space."<item id=\"$k\">\r\n".array2xml($v, $htmlon, $isnormal, $level + 1).$space."</item>\r\n";
+		}
+	}
+	$s = preg_replace("/([\x01-\x08\x0b-\x0c\x0e-\x1f])+/", ' ', $s);
+	return $level == 1 ? $s."</root>" : $s;
+}
 function save_config_file(){
 	global $_config;
 	if (!$_config) return;
