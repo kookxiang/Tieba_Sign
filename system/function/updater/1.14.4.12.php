b5e9a3c7e3c7cdd80a39df9b69deea0307d10be4
kookxiang <kookxiang@qq.com>
2014-04-14 21:00:27 +0800

    switch to dev channel if you come from old version

--- system/function/updater/1.14.4.12.php
+++ system/function/updater/1.14.4.12.php
@@ -1,6 +1,7 @@
 <?php
 if(!defined('IN_KKFRAME')) exit('Access Denied');
 saveSetting('template', 'default');
+saveSetting('channel', 'dev');
 saveSetting('version', '1.14.4.14');
 showmessage('成功更新到 1.14.4.14！', './');
 ?>
