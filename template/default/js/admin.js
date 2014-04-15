8e1a9251c398b1bf02dca2833e52de8d8e24ccc9
JerryLocke <i@jerrys.me>
2014-04-15 12:56:19 +0800

    Regulate preview box border

--- template/default/js/admin.js
+++ template/default/js/admin.js
@@ -214,7 +214,7 @@ function load_template(){
 			var obj = $(this);
 			var current = obj.attr('class')=='current';
 			var title = current ? '当前模板为 '+obj.attr('name') : '确定要切换到 '+obj.attr('name')+' 吗？';
-			var tips = '该模板作者为 <a href="'+obj.attr('site')+'" target="_blank">'+obj.attr('author')+'</a> ，当前版本为 '+obj.attr('version')+'<br><img style="width:390px;border:1px solid #dedede; border-radius:5px;margin:8px 0 -10px 0;" src="'+obj.find('img').eq(0).attr('src')+'" />';
+			var tips = '该模板作者为 <a href="'+obj.attr('site')+'" target="_blank">'+obj.attr('author')+'</a> ，当前版本为 '+obj.attr('version')+'<br><img style="width:390px;border:1px solid #979595; border-radius:3px;margin:8px 0 -10px 0;" src="'+obj.find('img').eq(0).attr('src')+'" />';
 			var tipsWindow = createWindow().setTitle(title).setContent(tips);
 			if (current) tipsWindow.addCloseButton('确定');
 			else tipsWindow.addButton('确定', function(){ msg_win_action("admin.php?action=set_template&template="+obj.attr('templateid')+"&formhash="+formhash); }).addCloseButton('取消');
