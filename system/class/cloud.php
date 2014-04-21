6e9c779a433c324d28c45db24ef6cbfde2154e54
kookxiang <i@ikk.me>
2014-04-21 13:02:48 +0800

    change sae api path

--- system/class/cloud.php
+++ system/class/cloud.php
@@ -4,7 +4,7 @@ if (!defined('IN_KKFRAME')) exit();
 class cloud {
 	const API_ROOT = 'http://api.ikk.me/v2/';
 	const API_ROOT_HTTPS = 'https://api.ikk.me/v2/';
-	const API_ROOT_SAE = 'http://sae.api.ikk.me/';
+	const API_ROOT_SAE = 'https://kksignapi.sinaapp.com/';
 	public static function init(){
 		list($id, $key) = self::_get_id_and_key();
 		if (!$id || !$key) define('CLOUD_NOT_INITED', true);
