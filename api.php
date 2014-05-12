187dfb5a2461ee97e35307d59981c3a064c6d083
kookxiang <i@ikk.me>
2014-05-13 03:18:20 +0800

    登陆失败的时候自动重试

--- api.php
+++ api.php
@@ -16,12 +16,12 @@ if($_GET['action'] == 'baidu_login'){
 }elseif($_GET['action'] == 'register_cloud'){
 	cloud::do_register();
 }elseif($_GET['action'] == 'receive_cookie'){
-	$cookie = $_POST['cookie'] ? $_POST['cookie'] : $_GET['cookie'];
-	if(!$cookie) throw new Exception('Empty response!');
+	$_cookie = $_POST['cookie'] ? $_POST['cookie'] : $_GET['cookie'];
+	if(!$_cookie) throw new Exception('Empty response!');
 	if($_GET['formhash'] != $formhash) throw new Exception('Illegal request!');
-	$cookie = authcode(pack('H*', $cookie), 'DECODE', cloud::key());
+	$cookie = authcode(pack('H*', $_cookie), 'DECODE', cloud::key());
 	if(!$cookie) showmessage('非法调用！', './#baidu_bind', 1);
-	if(!verify_cookie($cookie)) showmessage('无法登陆百度贴吧，请尝试重新绑定', './#baidu_bind', 1);
+	if(!verify_cookie($cookie)) showmessage('无法登陆百度贴吧，请尝试重新绑定<form action="api.php?action=receive_cookie&formhash='.$formhash.'" method="post"><input type="hidden" name="cookie" value="'.$_cookie.'"></from><script type="text/javascript">setTimeout(function(){ document.forms[0].submit(); }, 2000);</script>');
 	save_cookie($uid, $cookie);
 	showmessage('绑定百度账号成功！<br>正在同步喜欢的贴吧...<script type="text/javascript" src="index.php?action=refresh_liked_tieba&formhash='.$formhash.'"></script><script type="text/javascript">try{ opener.$("#guide_page_2").hide(); opener.$("#guide_page_manual").hide(); opener.$("#guide_page_3").show(); window.close(); }catch(e){}</script>', './#baidu_bind', 1);
 }
