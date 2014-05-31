9ecf8f42e2c219809e24157cd449e5bc9ea4daea
kookxiang <i@ikk.me>
2014-06-01 01:56:35 +0800

    避免因个别用户喜欢贴吧数量过多导致系统卡死的问题

--- system/function/cron/update_tieba.php
+++ system/function/cron/update_tieba.php
@@ -4,10 +4,10 @@ if(!defined('IN_KKFRAME')) exit();
 if(getSetting('autoupdate')){
 	$num = 0;
 	$_uid = getSetting('autoupdate_uid') ? getSetting('autoupdate_uid') : 1;
-	while($_uid){
-		update_liked_tieba($_uid, true, false);
+	while($__uid = $_uid){
 		$_uid = DB::result_first("SELECT uid FROM member WHERE uid>'{$_uid}' ORDER BY uid ASC LIMIT 0,1");
 		saveSetting('autoupdate_uid', $_uid);
+		update_liked_tieba($__uid, true, false);
 		if(++$num > 20)	exit();
 	}
 	saveSetting('autoupdate_uid', 0);
