5feaf06e759ffe88f7945270ebbd90b88eb39232
kookxiang <kookxiang@gmail.com>
2016-06-20 13:08:03 +0800

    Allow Message::show call i18n directly

--- Library/Helper/Message.php
+++ Library/Helper/Message.php
@@ -7,6 +7,7 @@
 namespace Helper;
 
 use Core\Filter;
+use Core\I18N;
 use Core\Response;
 use Core\Template;
 
@@ -21,7 +22,12 @@ class Message
     public static function show($text, $link = null, $timeout = 3)
     {
         Template::setView('Misc/Redirect');
-        Template::putContext('text', $text);
+        if (is_array($text)) {
+            array_unshift($text, $text[0]);     // Set fallback string
+            Template::putContext('text', call_user_func_array(array('I18N', 'parse'), $text));
+        } else {
+            Template::putContext('text', I18N::parse($text));
+        }
         Template::putContext('timeout', $timeout);
         Template::putContext('link', $link === null ? null : Response::generateURL($link));
         Filter::preRender();
