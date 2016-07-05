77cc33fa78342f6898dd329bded334181920515e
kookxiang <kookxiang@gmail.com>
2016-07-05 15:59:25 +0800

    Use PHP 5.6 built-in password functions

--- system/class/widget/widget_password.php
+++ system/class/widget/widget_password.php
@@ -3,6 +3,7 @@ if (!defined('IN_KKFRAME')) exit();
 class Widget_Password {
 	const ENCRYPT_TYPE_DEFAULT = 0;
 	const ENCRYPT_TYPE_ENHANCE = 1;
+	const ENCRYPT_TYPE_3MD5 = 2;
 	function verify($user, $password){
 		list($user_password, $encrypt_type) = explode('T', $user['password']);
 		if($encrypt_type == self::ENCRYPT_TYPE_DEFAULT){
@@ -10,10 +11,13 @@ class Widget_Password {
 		}elseif($encrypt_type == self::ENCRYPT_TYPE_ENHANCE){
 			$salt = substr(md5($user['uid'].$user['username'].ENCRYPT_KEY), 8, 16);
 			return $user_password == substr(md5(md5($password).$salt), 0, 30);
+		}elseif($encrypt_type == self::ENCRYPT_TYPE_3MD5){
+			return $user_password === md5(md5(md5($password)));
+		}else{
+			return password_verify($password, $user['password']);
 		}
 	}
 	function encrypt($user, $password){
-		$salt = substr(md5($user['uid'].$user['username'].ENCRYPT_KEY), 8, 16);
-		return substr(md5(md5($password).$salt), 0, 30).'T'.self::ENCRYPT_TYPE_ENHANCE;
+		return password_hash($password, PASSWORD_BCRYPT);
 	}
 }
