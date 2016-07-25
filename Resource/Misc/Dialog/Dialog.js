7311a9d85486b12d13675cd856c3c3a28f28c776
kookxiang <kookxiang@gmail.com>
2016-07-25 20:44:40 +0800

    Use new event module

--- Resource/Misc/Dialog/Dialog.js
+++ Resource/Misc/Dialog/Dialog.js
@@ -1,120 +1,116 @@
-"use strict";
 require("./Dialog.scss");
-var $ = require("jquery");
-var emitter = require("event-emitter");
-var dialogTpl = require("./Dialog.ejs");
-var Mask = require("./Mask");
+import $ from "jquery";
+import EventEmitter from "eventemitter3";
+import Mask from "./Mask";
+import DialogTemplate from "./Dialog.ejs";
 var autoIncrement = 0;
 
-var Dialog = function (options) {
-    var dialog = emitter({
-        id: autoIncrement++,
-        Title: "System Message",
-        Content: "",
-        Actions: [],
-        Element: null,
-        WithMask: true,
-        shown: false,
-        onDismiss: function (event) {
-            event.preventDefault();
-            dialog.hide();
-        },
-        show: function () {
-            dialog.emit("show");
-            dialog.shown = true;
-            if (dialog.WithMask) Mask.Show();
-            dialog.Element = $(dialogTpl({
-                Config: dialog
-            }));
-            $(document.body).append(dialog.Element);
-            dialog.Element.find("[data-action]").click(function (event) {
-                var action = $(this).data("action");
-                var methodName = "on" + action.charAt(0).toUpperCase() + action.substring(1);
-                if (dialog.hasOwnProperty(methodName)) {
-                    dialog[methodName](event);
-                }
-            });
-            dialog.reposition();
-            $(window).on("resize", dialog.reposition);
-            dialog.emit("after_show");
-            return dialog;
-        },
-        hide: function () {
-            dialog.emit("hide");
-            if (dialog.WithMask) Mask.Hide();
-            $(window).off("resize", dialog.reposition);
-            dialog.Element.addClass("hide");
-            setTimeout(function () {
-                dialog.emit("after_hide");
-            }, 125);
-            return dialog;
-        },
-        reposition: function () {
-            var width = window.innerWidth;
-            var height = window.innerHeight;
-            dialog.Element.css("left", (width - dialog.Element.outerWidth()) / 2);
-            dialog.Element.css("top", (height - dialog.Element.outerHeight()) / 2);
-        },
-        destroy: function () {
-            if (dialog.shown) {
-                dialog.Element.remove();
-            }
-            dialog = null;
-        },
-        before: function (event, callback) {
-            dialog.on(event, callback);
-            return dialog;
-        },
-        after: function (event, callback) {
-            dialog.on("after_" + event, callback);
-            return dialog;
+class Dialog extends EventEmitter {
+    constructor(config) {
+        super({});
+        this.id = autoIncrement++;
+        this.Title = "System Message";
+        this.Content = "";
+        this.Actions = [];
+        this.Element = null;
+        this.WithMask = true;
+        this.shown = false;
+        for (var key in config) {
+            this[key] = config[key];
         }
-    });
-    for (var key in options) {
-        dialog[key] = options[key];
     }
-    return dialog;
-};
-
-Dialog.ShowMessage = function (content, title, callback) {
-    return new Dialog({
-        Title: title || __("Dialog.DefaultTitle"),
-        Content: content,
-        Actions: [{
-            Action: "dismiss",
-            Label: __("Dialog.Buttons.OK"),
-            Default: true
-        }],
-        onDismiss: function (event) {
-            event.preventDefault();
-            this.hide();
-            if (callback) callback();
+    onDismiss(event) {
+        event.preventDefault();
+        this.hide();
+    }
+    show() {
+        let that = this;
+        this.emit("show");
+        this.shown = true;
+        if (this.WithMask) Mask.Show();
+        this.Element = $(DialogTemplate({
+            Config: this
+        }));
+        $(document.body).append(this.Element);
+        this.Element.find("[data-action]").click(function (event) {
+            var action = $(this).data("action");
+            var methodName = "on" + action.charAt(0).toUpperCase() + action.substring(1);
+            if (that.hasOwnProperty(methodName)) {
+                that[methodName](event);
+            }
+        });
+        this.reposition();
+        $(window).on("resize", this.reposition);
+        this.emit("after_show");
+        return this;
+    }
+    hide() {
+        this.emit("hide");
+        if (this.WithMask) Mask.Hide();
+        $(window).off("resize", this.reposition);
+        this.Element.addClass("hide");
+        setTimeout(() => this.emit("after_hide"), 125);
+        return this;
+    }
+    reposition() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        this.Element.css("left", (width - this.Element.outerWidth()) / 2);
+        this.Element.css("top", (height - this.Element.outerHeight()) / 2);
+    }
+    destroy() {
+        if (this.shown) {
+            this.Element.remove();
         }
-    }).after("hide", function () {
-        this.destroy();
-    }).show();
-};
-
-Dialog.Confirm = function (content, title, callback) {
-    return new Dialog({
-        Title: title || __("Dialog.DefaultTitle"),
-        Content: content,
-        Actions: [{
-            Action: "confirm",
-            Label: __("Dialog.Buttons.Confirm"),
-            Default: true
-        }, {
-            Action: "dismiss",
-            Label: __("Dialog.Buttons.Cancel")
-        }],
-        onConfirm: function (event) {
-            event.preventDefault();
-            this.hide();
-            if (callback) callback();
-        }
-    }).after("hide", function () {
-        this.destroy();
-    }).show();
-};
+        delete this;
+    }
+    before(event, callback) {
+        this.on(event, callback);
+        return this;
+    }
+    after(event, callback) {
+        this.on("after_" + event, callback);
+        return this;
+    }
+    static ShowMessage(content, title, callback) {
+        return new Dialog({
+            Title: title || __("Dialog.DefaultTitle"),
+            Content: content,
+            Actions: [{
+                Action: "dismiss",
+                Label: __("Dialog.Buttons.OK"),
+                Default: true
+            }],
+            onDismiss: function (event) {
+                event.preventDefault();
+                this.hide();
+                if (callback) callback();
+            }
+        }).after("hide", function () {
+            this.destroy();
+        }).show();
+    }
+    static Confirm(content, title, callback) {
+        return new Dialog({
+            Title: title || __("Dialog.DefaultTitle"),
+            Content: content,
+            Actions: [{
+                Action: "confirm",
+                Label: __("Dialog.Buttons.Confirm"),
+                Default: true
+            }, {
+                Action: "dismiss",
+                Label: __("Dialog.Buttons.Cancel")
+            }],
+            onConfirm: function (event) {
+                event.preventDefault();
+                this.hide();
+                if (callback) callback();
+            }
+        }).after("hide", function () {
+            this.destroy();
+        }).show();
+    }
+}
 
 module.exports = Dialog;
